5a1b9cc88d41431129f2c393bc1e684cc96a3404
a289459798 <289459798@qq.com>
2017-03-17 09:56:22 +0800

    增加两个exception

---
+++ Bijou/Exception/NoFoundException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangzy
+ * Date: 2017/3/17
+ * Time: 09:15
+ */
+
+namespace Bijou\Exception;
+
+
+class NoFoundException extends BijouException
+{
+
+    public function throwException()
+    {
+        $this->getResponse()->status(404);
+        $this->getResponse()->end(json_encode([
+            'code' => 404,
+            'message' => 'Not Found'
+        ]));
+    }
+
+}
